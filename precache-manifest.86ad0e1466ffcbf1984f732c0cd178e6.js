fe84a4774a5f1233093e9ffc72da6ed8f93f7d4f
joel sithivong <joelsithivong@MacBook-Pro-de-joel.local>
2020-02-07 21:11:23 +0100

    Updates

---
+++ precache-manifest.86ad0e1466ffcbf1984f732c0cd178e6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1728d260d9a4034c22b21aa8ab11854",
+    "url": "/todolist2/index.html"
+  },
+  {
+    "revision": "bb8c18ae934798a896b4",
+    "url": "/todolist2/static/css/2.9f735232.chunk.css"
+  },
+  {
+    "revision": "5821062675e232a80eb5",
+    "url": "/todolist2/static/css/main.f5d461c0.chunk.css"
+  },
+  {
+    "revision": "bb8c18ae934798a896b4",
+    "url": "/todolist2/static/js/2.88b5961c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/todolist2/static/js/2.88b5961c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5821062675e232a80eb5",
+    "url": "/todolist2/static/js/main.189f3f2b.chunk.js"
+  },
+  {
+    "revision": "8ce1c22a92b6b482a880",
+    "url": "/todolist2/static/js/runtime-main.34bdba5e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todolist2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todolist2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todolist2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todolist2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todolist2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/todolist2/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/todolist2/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/todolist2/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/todolist2/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/todolist2/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
